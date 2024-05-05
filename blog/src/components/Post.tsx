ef1677c44d166395790f2c5a8037f31d028bd8ec
threemonitors <0501402988@ukr.net>
2024-05-05 19:54:51 +0300

    added exercise 37 - part 2

--- blog/src/components/Post.tsx
+++ blog/src/components/Post.tsx
@@ -1,91 +1,67 @@
 // Exersise 37
 // Перепишіть функціональний компонент Post.tsx, використовуючи хуки useState, useEffect, useContext, createContext.
-
 import {useState, useEffect, useContext, createContext} from 'react';
 
-// Потрібно створити компонент PostMain
+const Post = () => {
+    // Потрібно створити контекст PostContext
+    const PostContext = createContext("");
 
-// Компонент Post повинен повертати наступне:
-
-//   return (
-//       <PostContext.Provider value={post}>
-//           <Layout>
-//           </Layout>
-//       </PostContext.Provider>
-//     );
-
-
-// Потрібно створити контекст PostContext
-export const PostContext = createContext('');
-
-// Потрібно створити компонент Layout
-const Layout = ({children}) => {
-    return (
-        <div>
-            <Header />
-            <main>
-                {children}
-            </main>
-        </div>
-    )
-}
-
-// Потрібно створити компонент Header
-const Header = () => {
-    return (
-        <header>
-            <PostTitle />
-        </header>
-    )
-}
-
-// Потрібно створити компонент PostTitle
-const postHeader = {
-    postName: "Our blog",
-    postDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi culpa odio asperiores a tempore."
-}
-
-const PostTitle = () => {
-    const postHeader = useContext(PostContext);
-    <>
-        <h2>{postHeader.postName}</h2>
-        return <p>{postHeader.postDescription}</p>;
-    </>
-}
-
-// Потрібно створити компонент PostMain
-const PostMain = () => {
-    const post = useContext(PostContext);
-    return (
-        <div>{post.content}</div>
-    )
-}
-
-function Post() {
-
-    const [post, setPost] = useState([]);
-    // const [likes, setLikes] = useState(0);
-    const url = 'https://my-json-server.typicode.com/three-monitors/db';
-
-    useEffect(() => {
-
-        const fetchData = async () => {
-            const data = await (await fetch(url)).json();
-            console.log(data);
-            setPost(data);
-        };
-        fetchData();
-    }, []);
-
-    // function likeThis() {
-    //     setLikes(likes + 1);
-    // }
-
-    return (
-        <PostContext.Provider value={post}>
-            <Layout>
+    // Потрібно створити компонент Layout
+    const Layout = ({children}) => {
+        return (
+            <div>
                 <Header />
+                <main>
+                    {children}
+                </main>
+            </div>
+        )
+    }
+
+    // Потрібно створити компонент Header
+    const Header = () => {
+        return (
+            <header>
                 <PostTitle />
+            </header>
+        )
+    }
+
+    // Потрібно створити компонент PostTitle
+    const PostTitle = () => {
+        return (
+            <>
+                <h2>{postHeader.postName}</h2>
+                <p>{postHeader.postDescription}</p>
+            </>
+        )
+    }
+    
+    const postHeader = {
+        postName: "Our blog",
+        postDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi culpa odio asperiores a tempore."
+    }
+
+    // Потрібно створити компонент PostMain
+    const PostMain = () => {
+        const post = useContext(PostContext);
+        return (
+            <div>{post.content}</div>
+        )
+    }
+
+    // Компонент Post повинен повертати наступне:
+
+    // return (
+    //     <PostContext.Provider value={post}>
+    //         <Layout>
+    //         </Layout>
+    //     </PostContext.Provider>
+    // );
+
+    return (
+        <PostContext.Provider value={postHeader}>
+            <Layout>
                 <PostMain />
             </Layout>
         </PostContext.Provider>
